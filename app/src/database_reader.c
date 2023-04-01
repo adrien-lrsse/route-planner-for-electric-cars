d2a6e7897416e73de41596ccf496836e99a3cf7e
Mathis Mangold <mathis.mangold@telecomnancy.eu>
2023-04-01 16:28:54 +0200

    database_reader.c & .h

--- app/src/database_reader.c
+++ app/src/database_reader.c
@@ -1,24 +1,24 @@
-#include <database_reader.h>
+#include "database_reader.h"
 
 database_t * open_database(const char *filename) { // ouvre la connexion vers la base de données
    sqlite3 *db;
    int rc;
 
-   rc = sqlite3_open16(filename, &db);
+   rc = sqlite3_open(filename, &db);
 
-   database_t * database;
-   database->db = db;
-   database->stmt = NULL;
+   struct database_t * my_database = malloc(sizeof(struct database_t));
+   my_database->db = db;
+   my_database->stmt = NULL;
 
    if (rc) {
       fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-      database->opened_corectly = false;
+      my_database->opened_correctly = false;
    } else {
       fprintf(stderr, "Opened database successfully\n");
-      database->opened_corectly = true;
+      my_database->opened_correctly = true;
    }
 
-   return database;
+   return my_database;
 }
 
 int close_database(database_t * database) { // ferme la connexion vers la base de données et supprime la structure database_t
@@ -35,15 +35,59 @@ int close_database(database_t * database) { // ferme la connexion vers la base d
    return 1;
 }
 
-int request_database(database_t * database, char * request) { // exécute la requête donnée en argument
-   int rc = sqlite3_prepare16_v2(database->db, request, -1, &database->stmt, NULL);
+int prepare_request_database(database_t * database, char * request) {
+   int rc = sqlite3_prepare_v2(database->db, request, -1, &database->stmt, NULL);
 
    if (rc) {
-      fprintf(stderr, "Failed to prepare statement: %s", sqlite3_errmsg(database->db));
+      fprintf(stderr, "Failed to prepare statement: %s\n", sqlite3_errmsg(database->db));
+      return 0;
 
    } else {
-      fprintf(stderr, "Prepared statement successfully");
+      fprintf(stderr, "Prepared statement successfully\n");
    }
 
    return 1;
+}
+
+int request_database(database_t * database) {
+   int rc = sqlite3_step(database->stmt);
+
+   printf("--------------------\n");
+   printf("Begin of all returned values\n");
+   printf("%s\n", sqlite3_column_text(database->stmt, 0));
+
+   while ((rc = sqlite3_step(database->stmt)) == SQLITE_ROW) {
+      printf("%s\n", sqlite3_column_text(database->stmt, 0));
+   }
+
+   printf("End of all returned values\n");
+   printf("--------------------\n");
+
+   if (rc != SQLITE_DONE) {
+      fprintf(stderr, "Failed to execute statement: %s\n", sqlite3_errmsg(database->db));
+      return 0;
+   } else {
+      fprintf(stderr, "Executed statement successfully\n");
+   }
+
+   return 1;
+}
+
+int end_request_database(database_t * database) {
+   sqlite3_finalize(database->stmt);
+   return 1;
+}
+
+int main() {
+   database_t * database = open_database("./../data/database.db");
+   if (!database->opened_correctly) {
+      exit(0);
+   }
+   printf("Version of sqlite3 : %s\n", sqlite3_libversion());
+   char* sql_command = "SELECT * FROM bornes where id_unique like 13;\n";
+   prepare_request_database(database, sql_command);
+   request_database(database);
+   end_request_database(database);
+   close_database(database);
+   return 0;
 }
--- app/src/database_reader.h
+++ app/src/database_reader.h
@@ -4,13 +4,17 @@
 #include <stdio.h>
 #include <sqlite3.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
-database_t * open_database(const char *filename);
-int close_database(database_t * database);
 typedef struct database_t {
    sqlite3 *db;
    sqlite3_stmt *stmt;
-   bool opened_corectly;
+   bool opened_correctly;
 } database_t;
+database_t * open_database(const char *filename);
+int close_database(database_t * database);
+int prepare_request_database(database_t * database, char * request);
+int request_database(database_t * database);
+int end_request_database(database_t * database);
 
 #endif /* DATABASE_READER_H */
